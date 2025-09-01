7f287fbc74a3bfce480b33e4c3eb42febc28e830
JeanR1802 <rs.cedora@gmail.com>
2025-09-01 07:29:15 -0700

    Fix next.config file for Verceel

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+// lib/auth.ts
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import GoogleProvider from "next-auth/providers/google"
+import { type NextAuthOptions } from "next-auth"
+
+const prisma = new PrismaClient()
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session
+    },
+  },
+}
