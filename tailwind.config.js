8294f85e0f65de68ae0a221a51db8f11700a7a90
JeanR1802 <rs.cedora@gmail.com>
2025-09-01 07:52:34 -0700

    arreglado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{ts,tsx,js,jsx}',
+    './components/**/*.{ts,tsx,js,jsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
