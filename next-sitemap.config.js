18467908b950cea63ed8062a19d9e762d0896b39
JeanR1802 <rs.cedora@gmail.com>
2025-09-01 16:42:52 -0700

    arreglado

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://gestularia.com',
+  generateRobotsTxt: false, // ya tienes tu propio robots.txt
+  sitemapSize: 5000,
+};
