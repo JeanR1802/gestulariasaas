38b011941dff45d56a93353935de319e77db0620
JeanR1802 <rs.cedora@gmail.com>
2025-09-02 01:31:22 -0700

    arreglado

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,12 @@
-// prisma.ts (o el nombre de tu archivo)
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-import { PrismaClient } from '@prisma/client/edge' // Fíjate en el '/edge'
-import { withAccelerate } from '@prisma/extension-accelerate' // Importa la extensión
+let prisma;
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient().$extends(withAccelerate());
+} else {
+  prisma = new PrismaClient();
+}
 
-export default prisma
+export default prisma;
--- prisma/migrations/20250902082708_init/migration.sql
+++ prisma/migrations/20250902082708_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT,
-    "password" TEXT NOT NULL,
+    "password" TEXT,
     "apiKey" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
