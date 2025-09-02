7d3fbfbbbc1360958b82c35a53b719fac6cce7a8
JeanR1802 <rs.cedora@gmail.com>
2025-09-02 00:58:07 -0700

    arreglado

--- pages/api/sites/route.ts
+++ pages/api/sites/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma'
 
 async function getUserFromApiKey(request: Request) {
   const apiKey = request.headers.get("Authorization")?.replace("Bearer ", "");
