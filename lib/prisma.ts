014c74410fb4f8c0f64c8874e789476940089c86
JeanR1802 <rs.cedora@gmail.com>
2025-09-02 01:34:30 -0700

    arreglado

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-let prisma;
+// Declara la variable con el tipo correcto de PrismaClient
+let prisma: PrismaClient;
 
+// Usa una condición para el entorno de producción
 if (process.env.NODE_ENV === 'production') {
+  // Inicializa Prisma con la extensión de Accelerate
   prisma = new PrismaClient().$extends(withAccelerate());
 } else {
+  // Inicializa un cliente normal para el entorno de desarrollo
   prisma = new PrismaClient();
 }
 
