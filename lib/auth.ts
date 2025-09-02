1f7438908d25f504715fb553c8e6dd883e3ae912
JeanR1802 <rs.cedora@gmail.com>
2025-09-01 18:51:45 -0700

    arreglado

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,7 +5,7 @@ import { type NextAuthOptions } from "next-auth";
 import prisma from "@/lib/prisma"; // Cambio: Importamos el cliente centralizado
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma), // Cambio: Usamos el cliente importado
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -14,6 +14,10 @@ export const authOptions: NextAuthOptions = {
   ],
   pages: {
     signIn: "/login",
+  },
+  session: {
+    maxAge: 24 * 60 * 60, // La sesión dura 1 día
+    updateAge: 60 * 60,   // Solo refresca la sesión cada 1 hora
   },
   callbacks: {
     session({ session, user }) {
